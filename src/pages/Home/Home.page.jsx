4e42546361b11fc4a3fa49975bdc946faffadddd
Edward <hvu160699@gmail.com>
2021-07-02 00:42:10 +0700

    Implement Video action for Home Page

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,38 +1,29 @@
 import React, { useEffect, useCallback } from 'react';
 
-import { $getVideos } from '../../api/videos';
-
 import GridVideo from '../../components/GridVideo';
 import Container from '../../components/Common/Container';
 import { useVideoContext } from '../../providers/Video';
+import { fetchVideos } from '../../providers/Video/Video.actions';
 
 const HomePage = () => {
   const { state, dispatch } = useVideoContext();
 
-  const fetchVideos = useCallback(async () => {
-    dispatch({ type: 'VIDEO/FETCH_PROCESSING' });
-    try {
-      const queryData = {
-        q: state.q,
-        chart: 'mostPopular',
-        regionCode: 'vn',
-        part: ['snippet'],
-        type: 'video',
-        maxResults: 5,
-      };
+  const handleFetchVideos = useCallback(async () => {
+    const queryData = {
+      q: state.q,
+      chart: 'mostPopular',
+      regionCode: 'vn',
+      part: ['snippet'],
+      type: 'video',
+      maxResults: 5,
+    };
 
-      const res = await $getVideos(queryData);
-
-      if (res.items.length > 0)
-        dispatch({ type: 'VIDEO/FETCH_LIST_SUCCESS', payload: res.items });
-    } catch (err) {
-      dispatch({ type: 'VIDEO/FETCH_FAILURE' });
-    }
+    dispatch(fetchVideos(queryData));
   }, [state.q, dispatch]);
 
   useEffect(() => {
-    fetchVideos();
-  }, [fetchVideos]);
+    handleFetchVideos();
+  }, [handleFetchVideos]);
 
   return (
     <Container className="mx-auto">
